dffbf1fa0dee9610a53dffe80ff2494cbacce048
William Tan <35724132+tankwong@users.noreply.github.com>
2025-08-17 18:59:40 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from snowflake.snowpark.context import get_active_session
 import pandas as pd
 import matplotlib.pyplot as plt
-from snowflake.cortex import complete
+#from snowflake.cortex import complete
 
 # Initialize the Streamlit app
 st.title("Avalanche Streamlit App")
